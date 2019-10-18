75da756a7ff66c73a3e954a7e26d0e5733b0842f
Joseph Moshood <gladstargtschool6@gmail.com>
2019-10-18 12:08:33 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -18,44 +18,6 @@ function emitChatMessage(chat_message) {
 }
 
 export { subscribeToChat, emitChatMessage, unsubscribeFromChat };
-{
-  "project_info": {
-    "project_number": "61216389060",
-    "firebase_url": "https://marine-infinity-244511.firebaseio.com",
-    "project_id": "marine-infinity-244511",
-    "storage_bucket": "marine-infinity-244511.appspot.com"
-  },
-  "client": [
-    {
-      "client_info": {
-        "mobilesdk_app_id": "1:61216389060:android:a34706310a2c4f78",
-        "android_client_info": {
-          "package_name": "com.gladstar.gladstargtschool"
-        }
-      },
-      "oauth_client": [
-        {
-          "client_id": "61216389060-4u847t7dkd80gbvrlk1turov9npmgpmt.apps.googleusercontent.com",
-          "client_type": 3
-        }
-      ],
-      "api_key": [
-        {
-          "current_key": "AIzaSyCVgTBrHMK808-huXDbEsx5nDmyPwbxiLI"
-        }
-      ],
-      "services": {
-        "appinvite_service": {
-          "other_platform_oauth_client": [
-            {
-              "client_id": "61216389060-4u847t7dkd80gbvrlk1turov9npmgpmt.apps.googleusercontent.com",
-              "client_type": 3
-            }
-          ]
-        }
-      },
-      "admob_app_id": "ca-app-pub-7518766331039540~2240433268"
-    }
-  ],
-  "configuration_version": "1"
-}
+
+api_key = "AIzaSyCVgTBrHMK808-huXDbEsx5nDmyPwbxiLI"
+        
